930e7f9e774ae9829bafd49d11392fee8d177324
Tim Kadlec <tim@timkadlec.com>
2019-10-18 10:04:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Netlify SpeedCurve Deploy Plugin
+
+After a succesful build, tell SpeedCurve you've deployed and trigger a round of testing.
+
+## Usage
+In your plugins section of the configuration file, add a reference tot he 
---
+++ index.js
@@ -0,0 +1,39 @@
+const {
+    env: {
+        //Your SpeedCurve API Key (Admin > Teams)
+        SPEEDCURVE_API_KEY,
+        SPEEDCURVE_SITE_ID
+    }
+} = require('process')
+
+const fetch = require('node-fetch');
+
+module.exports = {
+    async finally() {
+        console.log('Preparing to trigger SpeedCurve tests');
+
+        fetch('https://api.speedcurve.com/v1/deploys', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(SPEEDCURVE_API_KEY + ':' + 'x').toString('base64'),
+                "Content-type": "application/json",
+                "Accept": "application/json",
+                "Accept-Charset": "utf-8"
+            },
+            body: JSON.stringify({
+                site_id: SPEEDCURVE_SITE_ID
+            })
+        })
+        .then(function (data) {
+            if (data.status == 200) {
+                console.log('SpeedCurve test submitted!');
+            } else {
+                console.log('SpeedCurve test couldn\'t be submitted. Status: ' + data.statusText);
+            }
+        })
+        .catch(function (error) {
+            console.log('Error: ', error);
+        });
+        
+    }
+}
