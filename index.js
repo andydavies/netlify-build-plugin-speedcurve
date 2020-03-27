af6b950dde8a5b4bc27adc92e3657cdf1a12ba36
ehmicky <ehmicky@gmail.com>
2020-03-27 20:06:29 +0100

    Upgrade to latest version of Netlify Build

--- index.js
+++ index.js
@@ -11,33 +11,31 @@ const {
 const fetch = require('node-fetch');
 
 module.exports = {
-    name: 'netlify-build-plugin-speedcurve',
-    async onSuccess() {
+    async onSuccess({ utils: { build: { failPlugin, failBuild } } }) {
         console.log('Preparing to trigger SpeedCurve tests');
 
-        fetch('https://api.speedcurve.com/v1/deploys', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Basic ' + Buffer.from(SPEEDCURVE_API_KEY + ':' + 'x').toString('base64'),
-                "Content-type": "application/json",
-                "Accept": "application/json",
-                "Accept-Charset": "utf-8"
-            },
-            body: JSON.stringify({
-                site_id: SPEEDCURVE_SITE_ID,
-                note: COMMIT_REF
+        try {
+            const { status, statusText } = await fetch('https://api.speedcurve.com/v1/deploys', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Basic ' + Buffer.from(SPEEDCURVE_API_KEY + ':' + 'x').toString('base64'),
+                    "Content-type": "application/json",
+                    "Accept": "application/json",
+                    "Accept-Charset": "utf-8"
+                },
+                body: JSON.stringify({
+                    site_id: SPEEDCURVE_SITE_ID,
+                    note: COMMIT_REF
+                })
             })
-        })
-        .then(function (data) {
-            if (data.status == 200) {
-                console.log('SpeedCurve test submitted!');
-            } else {
-                console.log('SpeedCurve test couldn\'t be submitted. Status: ' + data.statusText);
+
+            if (status != 200) {
+                return failPlugin('SpeedCurve test couldn\'t be submitted. Status: ' + statusText);
             }
-        })
-        .catch(function (error) {
-            console.log('Error: ', error);
-        });
-        
+
+            console.log('SpeedCurve test submitted!');
+        } catch (error) {
+            return failBuild('SpeedCurve test failed', { error })
+        }
     }
 }
